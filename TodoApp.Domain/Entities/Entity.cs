3a0cc3379dfc2071fa8d72d524c4ef2901bfdacb
Igor Santiago <contato.igorsantiago@outlook.com>
2023-07-10 16:52:16 -0300

    Entity and TodoItem completed

---
+++ TodoApp.Domain/Entities/Entity.cs
@@ -0,0 +1,16 @@
+namespace TodoApp.Domain.Entities;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; private set; }
+
+    public bool Equals(Entity? other)
+    {
+        return Id == other?.Id;
+    }
+}
